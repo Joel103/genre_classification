10581490a09a0d0f342f220b1064a126dc176447
Wassim Boubaker <wassim.boubaker@nttdata.com>
2021-02-17 21:16:19 +0100

    quick push for plotting

--- ann_utils.py
+++ ann_utils.py
@@ -4,14 +4,17 @@ import annoy
 from annoy import AnnoyIndex
 from collections import Counter
 from sklearn.manifold import TSNE
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
 class ANN():
     '''
     (super sorry, but I don't really have time. Shame on me.)
     '''
     
-    def __init__(self, base_population_path, label_path, experimental_size,
+    def __init__(self, base_population_path, label_path, filename_path, experimental_size,
                  metric='angular', n_trees=10):
+        self.filename_path = filename_path
         self.base_population_path = base_population_path
         self.label_path = label_path
         self.experimental_size = experimental_size
@@ -19,14 +22,11 @@ class ANN():
         self.base_population = None
         self.labels = None
         self.n_trees = n_trees
+        self.genre_names = ['Hip-Hop', 'Pop', 'Folk', 'Experimental', 'Rock',
+                            'International', 'Electronic', 'Instrumental']
         
         self._load_population() # load files
         self.embedding_size = self.base_population.shape[1]
-                
-        print('working on TSNE. Might take a while')
-        self.tsne = TSNE(n_components=2, random_state=0)
-        self.tsne_output = self.tsne.fit_transform(self.base_population)
-        print('Oof. Done with TSNE.')
         
         self.annoy_index = AnnoyIndex(self.embedding_size, metric)
         
@@ -37,6 +37,12 @@ class ANN():
         
         # build 
         self.annoy_index.build(n_trees)
+        
+    def _prepare_tsne(self):
+        print('working on TSNE. Might take a while')
+        self.tsne = TSNE(n_components=2, random_state=0)
+        self.tsne_output = self.tsne.fit_transform(self.base_population)
+        print('Oof. Done with TSNE.')
         
     def add_reindex(self, new_embeddings, add_to_base=False):
         '''
@@ -55,12 +61,17 @@ class ANN():
         '''
         see if labels assigned by voting really match closest neighbors 
         '''
-        predictions = []
+        self.predictions = []
         for embed_vec in self.base_population:
-            predictions.append(self.assign_label(embed_vec))
-        self.top_x = np.sum([label in pred for label, pred in zip(self.labels, predictions)])/len(self.labels)
-        self.assigned_classes = np.sum([label == pred[0] for label, pred in zip(self.labels, predictions)]) /len(self.labels)
-        print(self._evaluation_report(top_x)) 
+            self.predictions.append(self.assign_label(embed_vec))
+        self.top_x = np.sum([label in pred for label, pred in zip(self.labels, self.predictions)])/len(self.labels)
+        self.assigned_classes = np.sum([label == pred[0] for label, pred in zip(self.labels, self.predictions)]) /len(self.labels)
+        print(self._evaluation_report(top_x))
+        
+        unit_predictions = [pred[0] for pred in self.predictions]
+        
+        self.classification_report = classification_report(self.labels, unit_predictions, target_names=self.genre_names)
+        self.confusion_matrix = confusion_matrix(self.labels, unit_predictions, labels=range(len(self.genre_names)))
     
     def _evaluation_report(self, top_x):
         out_str = f'classification acc: {100 * np.round(self.assigned_classes, 2)}% \n'
@@ -73,6 +84,10 @@ class ANN():
         '''
         predictions = self.annoy_index.get_nns_by_vector(sample_vec, knn, include_distances=False)
         return np.array(Counter(self.labels[predictions]).most_common())[:top_x,0]
+    
+    def assign_filename(self, sample_vec, knn=30, top_x=1):
+        predictions = self.annoy_index.get_nns_by_vector(sample_vec, knn, include_distances=False)
+        return np.array(Counter(self.filenames[predictions]).most_common())[:top_x,0]
     
     def _compute_confidence(self):
         '''
@@ -87,6 +102,8 @@ class ANN():
         plt.show()
     
     def plot_tsne(self, size):
+        
+        self._prepare_tsne()
         
         idx = np.random.choice(np.arange(len(self.labels)), size, replace=False)
         
@@ -103,7 +120,9 @@ class ANN():
         if self.experimental_size > -1:
             self.base_population = np.load(self.base_population_path, allow_pickle=True).squeeze()[:self.experimental_size]
             self.labels = np.load(self.label_path, allow_pickle=True)[:self.experimental_size]
+            self.filenames = np.load(self.filename_path, allow_pickle=True)[:self.experimental_size]
         else:
             self.base_population = np.load(self.base_population_path, allow_pickle=True).squeeze()
             self.labels = np.load(self.label_path, allow_pickle=True)
+            self.filenames = np.load(self.filename_path, allow_pickle=True)
         
